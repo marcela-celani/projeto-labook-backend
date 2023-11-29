b8d63702b16f37252fbf0f2d85251479f2e7564a
marcela-celani <marcelacelani.ti@gmail.com>
2023-11-29 01:08:53 -0300

    tipagens e classes

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,32 @@
-export class User {    
+export enum USER_ROLES {
+    NORMAL = "NORMAL",
+    ADMIN = "ADMIN"
+}
+
+export interface TokenPayload {
+    id: string,
+    name: string,
+    role: USER_ROLES
+}
+
+export interface UserDB {
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+    role: USER_ROLES,
+    created_at: string
+}
+
+export interface UserModel {
+    id: string,
+    name: string,
+    email: string,
+    role: USER_ROLES,
+    createdAt: string
+}
+
+export class User {
     constructor(
         private id: string,
         private name: string,
@@ -6,12 +34,12 @@ export class User {
         private password: string,
         private role: USER_ROLES,
         private createdAt: string
-    ) {}
+    ) { }
 
     public getId(): string {
         return this.id
     }
-    
+
     public setId(value: string): void {
         this.id = value
     }
@@ -67,3 +95,14 @@ export class User {
             created_at: this.createdAt
         }
     }
+    // para utilizar no front end
+    public toBusinessModel(): UserModel {
+        return {
+            id: this.id,
+            name: this.name,
+            email: this.email,
+            role: this.role,
+            createdAt: this.createdAt
+        }
+    }
+}
